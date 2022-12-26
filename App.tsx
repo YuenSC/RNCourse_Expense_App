bbd9d0f106d1803d3dad0dea8607f0967739abb9
Calvin Yuen <scyuenab@gmail.com>
2022-12-26 16:00:12 +0800

    feat: Add Basic Screen, Tab Navigation, Tab Icon

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { MaterialCommunityIcons, Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import AllExpenseScreen from "./screens/AllExpenseScreen";
+import RecentExpenseScreen from "./screens/RecentExpenseScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="All Expense"
+          component={AllExpenseScreen}
+          options={{
+            tabBarIcon: ({ color, focused, size }) => (
+              <MaterialCommunityIcons
+                name={focused ? "timer-sand-full" : "timer-sand-empty"}
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Recent Expense"
+          component={RecentExpenseScreen}
+          options={{
+            tabBarIcon: ({ color, focused, size }) => (
+              <Ionicons
+                name={focused ? "ios-calendar-sharp" : "ios-calendar-outline"}
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/AllExpenseScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const AllExpenseScreen = () => {
+  return (
+    <View>
+      <Text>AllExpenseScreen</Text>
+    </View>
+  );
+};
+
+export default AllExpenseScreen;
+
+const styles = StyleSheet.create({});
