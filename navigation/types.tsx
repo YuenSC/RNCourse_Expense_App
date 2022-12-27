0873e2d3ed7aef9d9a545a82d16b5ca7ec28a388
Calvin Yuen <scyuenab@gmail.com>
2022-12-27 13:53:51 +0800

    docs: Add Comment in Navigation type

--- navigation/types.tsx
+++ navigation/types.tsx
@@ -5,24 +5,26 @@ import {
 } from "@react-navigation/native";
 import { StackScreenProps } from "@react-navigation/stack";
 
+/* RootStack Start */
 export type RootStackParamList = {
   Home: NavigatorScreenParams<HomeTabParamList>;
   ExpenseForm: { id?: string };
 };
-
 export type RootStackScreenProps<T extends keyof RootStackParamList> =
   StackScreenProps<RootStackParamList, T>;
+/* RootStack End*/
 
+/* HomeTab Start */
 export type HomeTabParamList = {
   "Recent Expense": undefined;
   "All Expense": undefined;
 };
-
 export type HomeTabScreenProps<T extends keyof HomeTabParamList> =
   CompositeScreenProps<
     BottomTabScreenProps<HomeTabParamList, T>,
     RootStackScreenProps<keyof RootStackParamList>
   >;
+/* HomeTab End */
 
 declare global {
   namespace ReactNavigation {
