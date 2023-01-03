024abf49cda8fc85793ceddc811d360865ff8f7a
Calvin Yuen <scyuenab@gmail.com>
2023-01-03 22:30:54 +0800

    feat: Edit mode in Form Screen

--- screens/ExpenseFormScreen.tsx
+++ screens/ExpenseFormScreen.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import { IRootStackScreenProps } from "@types";
+import React, { useLayoutEffect } from "react";
 import { Text, View } from "react-native";
 
-const ExpenseFormScreen = () => {
+const ExpenseFormScreen = ({
+  navigation,
+  route,
+}: IRootStackScreenProps<"ExpenseForm">) => {
+  const {
+    params: { id },
+  } = route;
+
+  const isEdit = !!id;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: isEdit ? "Edit Expense" : "Create Expense",
+    });
+  }, [isEdit, navigation]);
+
   return (
     <View>
       <Text>ExpenseFormScreen</Text>
+      <Text>id : {id}</Text>
     </View>
   );
 };
